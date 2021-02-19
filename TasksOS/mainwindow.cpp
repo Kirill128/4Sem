3d283799867fa9893c197dae58f68a56eddcb966
Kirill <liw.liw510@gmail.com>
2021-02-19 22:09:59 +0300

    some:

--- TasksOS/mainwindow.cpp
+++ TasksOS/mainwindow.cpp
@@ -9,19 +9,19 @@ MainWindow::MainWindow(QWidget *parent)
     widget=new QWidget();
     process=new QProcess(this);
     vLayout=new QVBoxLayout(this);
-    textEdit=new QTextEdit();
+   // textEdit=new QTextEdit();
 
-    vLayout->addWidget(textEdit);
-    widget->setLayout(vLayout);
-    widget->show();
-
-    this->setCentralWidget(widget);
     connect(process,SIGNAL(readyReadStandardOutput()),this,SLOT(showOut()));
     process->start("ps",QStringList{"-e","--format","uname pid %mem pri lwp nlwp cmd"});
 }
 
 MainWindow::~MainWindow()
 {
+    for(int i=0;i<processTable->rowCount();i++){
+        for(int j=0;j<processTable->columnCount();j++)
+        processTable->item(i,j);
+    }
+    delete this->processTable;
     delete ui;
 }
 QTableWidget* MainWindow::makeProcessTable(QStringList source,QStringList collumnNames){
@@ -29,22 +29,34 @@ QTableWidget* MainWindow::makeProcessTable(QStringList source,QStringList collum
     int collumns=collumnNames.count();
     QTableWidget* table=new QTableWidget(rows,collumns);
 
+    QStringListIterator rowIterator(source);
+    QString rowString;
     table->setHorizontalHeaderLabels(collumnNames);
     for(int i=0;i<rows;i++){
+        rowString=rowIterator.next();
+        QStringList list=rowString.split(' ');
+        QStringListIterator collumnIterator(list);
         for(int j=0;j<collumns;j++){
-
-            QTableWidgetItem* item=new QTableWidgetItem();
+            QTableWidgetItem* item=new QTableWidgetItem(collumnIterator.next());
             table->setItem(i,j,item);
         }
     }
-
-
     return table;
 }
 void MainWindow::showOut(){
-    this->textEdit->append(process->readAllStandardOutput());
+    //this->textEdit->append(process->readAllStandardOutput());
 
-  //  ui->textEdit->append(process->readAllStandardError());
+    this->processTable=makeProcessTable(QString(process->readAllStandardOutput()).split('\n'),QStringList{"uname", "pid", "%mem", "pri", "lwp", "nlwp", "cmd"});
+
+    //this->processTable=MainWindow::makeProcessTable(QStringList{"first second","second first"},QStringList{"name","demo"});
+
+//    this->vLayout->addWidget(processTable);
+//    this->widget->setLayout(vLayout);
+//    this->widget->show();
+
+    this->setCentralWidget(processTable);
+
+
 }
 
 QProcess *MainWindow::getProcess() const
