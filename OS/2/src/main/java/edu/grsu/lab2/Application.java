c2792bb0caa9586e90384e324608102975003799
Kirill <liw.liw510@gmail.com>
2021-03-17 15:52:37 +0300

    2 os done

--- OS/2/src/main/java/edu/grsu/lab2/Application.java
+++ OS/2/src/main/java/edu/grsu/lab2/Application.java
@@ -5,13 +5,59 @@ import java.util.Date;
 import java.util.List;
 
 public class Application {
-    public static void main(String[] args) {
-        arrayProcess(makeArray(10),10);
+    public static void main(String[] args) throws InterruptedException {
+        int k=1000;
+        int iMax=10;
+        List<List<Double>> times=new ArrayList<>();
+        long middleTime;
+
+        for(int threadsCount = 1;threadsCount<100;threadsCount++) {
+            middleTime = 0;
+            for (int i = 0; i < iMax; i++) {
+                List<List<Double>> lists = diviteToArrays(makeArray(10), threadsCount);
+                middleTime += threadsExec(lists, k);
+            }
+            System.out.println("Threads count: "+threadsCount+", Middle time:"+middleTime / iMax);
+            List<Double> time=new ArrayList<>(2);
+            time.add(threadsCount);
+            time.add(middleTime/iMax);
+        }
+
+        middleTime = 0;
+        for (int i = 0; i < iMax; i++) {
+            List<List<Double>> lists = diviteToArrays(makeArray(10), 1);
+            middleTime += threadsExec(lists, k);
+        }
+        System.out.println("Threads count: 1, Middle time:"+middleTime / iMax);
+
     }
+
+    public static long threadsExec(List<List<Double>> arrays,int k) throws InterruptedException {
+        long startTime=System.nanoTime();
+        Thread[]threads=new Thread[arrays.size()];
+        for(int i=0;i<threads.length;i++){
+            int indx=i;
+            threads[i]=new Thread(new Runnable() {
+                @Override
+                public void run() {
+//                    System.out.println(indx+" Started");
+                    arrayProcess(arrays.get(indx),k);
+//                    System.out.println(indx+" Ended");
+                }
+            });
+        }
+        for(int i=0;i<threads.length;i++){
+            threads[i].start();
+        }
+        for(int i=0;i<threads.length;i++){
+            threads[i].join();
+        }
+        return System.nanoTime()-startTime;
+    }
+
     public static void arrayProcess(List<Double> array,int k){
         List<Double> finalArr=new ArrayList<>();
         double currentEl;
-        long startTime=System.nanoTime();
         for(int i=0;i<array.size();i++){
             currentEl=array.get(i);
             for(int j=0;j<k;j++){
@@ -19,10 +65,30 @@ public class Application {
             }
             finalArr.add(currentEl);
         }
-        long endTime=System.nanoTime();
-        System.out.println("Res of time(Nano sek.):"+(endTime-startTime));
     }
-    public static void splitTo
+    public static List<List<Double>> diviteToArrays(List<Double> mainArr,int subArrCount) {
+        List<List<Double>> result = new ArrayList<>();
+        if (subArrCount <= mainArr.size()){
+            int elInArr = mainArr.size() / subArrCount;
+            int elIndx = 0;
+            for (int subArrIndx = 0; subArrIndx < subArrCount - 1; subArrIndx++) {
+                List<Double> one = mainArr.subList(elIndx, elIndx = (elIndx + elInArr));
+                result.add(one);
+            }
+            result.add(mainArr.subList(elIndx, mainArr.size()));
+        }else
+        {
+            int i;
+            for(i=0;i<mainArr.size();i++){
+                List<Double> one=new ArrayList<Double>();
+                one.add(mainArr.get(i));
+                result.add(one);
+            }
+            for(;i<subArrCount;i++)
+                result.add(new ArrayList<>());
+        }
+        return  result;
+    }
     public static List<Double> makeArray(int length){
         List<Double> arr=new ArrayList<>(length);
         for(int i=0;i<length;i++) {
