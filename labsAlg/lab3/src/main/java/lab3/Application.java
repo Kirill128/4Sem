febfb01920a61dcc715a7cefdc00f1cf9ebf9995
Kirill <liw.liw510@gmail.com>
2021-04-01 15:06:41 +0300

    s

---
+++ labsAlg/lab3/src/main/java/lab3/Application.java
@@ -0,0 +1,16 @@
+package lab3;
+
+public class Application {
+    public static void main(String[] args) {
+        //Right variant
+        String x="01110110";
+        String y="AABBABABBBAAAA";
+        //False variant
+        String x1="010";
+        String y1="BBBAB";
+
+
+    }
+
+
+}
