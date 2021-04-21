f6dcecd1352bb464d93a840f808994eaadf10639
Kirill128 <56486184+Kirill128@users.noreply.github.com>
2021-04-21 14:35:37 +0300

    It works, lab 3 alg

--- labsAlg/3/src/main/java/lab/Main.java
+++ labsAlg/3/src/main/java/lab/Main.java
@@ -1,25 +1,47 @@
-package lab;
+package some;
+
+import java.util.Scanner;
 
 public class Main {
-    public static char[] arrayI=new char[]{'0','0','1','1','0'};
-    public static char[] arrayJ=new char[]{'A','A','A','A','B','B','A','A'};
+//    public static char[] arrayI=new char[]{'1','1','0','0','1','1'};
+//    public static char[] arrayJ=new char[]{'A','A','A','B','B','A','A','A','B','B','B'};
+    public static char[] arrayI;
+    public static char[] arrayJ;
+
+//    public static char[] arrayI=new char[]{'0','0','1','1','0'};
+//    public static char[] arrayJ=new char[]{'A','A','A','A','B','B','A','A'};
     public static int globalI=-1;
 
     public static void main(String[] args) {
-        System.out.printf("Can: "+canConvert(new char[]{' ',' ',' ',' ',' ',' ',' ',' '},-1,-1));
+        System.out.println("Input STR 1 (0101010...)");
+        arrayI=inputString();
+        System.out.println("Input STR 2 (ABABABA...)");
+        arrayJ=inputString();
+
+        char[] postArray=new char[arrayJ.length];
+        for(int i=0;i<postArray.length;i++)postArray[i]=' ';
+        System.out.printf("Can: "+canConvert(postArray,-1,-1));
     }
+
+    public static char[] inputString(){
+        Scanner scanner=new Scanner(System.in);
+        return scanner.next().toCharArray();
+    }
+
     public static boolean canConvert(char[] receiveArray,int firstXPositionOfReceiveArray,int lastXPositionOfReceiveArray){
         globalI++;
-        char[] postArray=new char[]{' ',' ',' ',' ',' ',' ',' ',' '};
+        char[] postArray=new char[receiveArray.length];
+        for(int i=0;i<postArray.length;i++)postArray[i]=' ';
 
         int j=firstXPositionOfReceiveArray+1;
         int startOfNewInterval=j;
         boolean startFound=false;
         boolean wasInInterval=false;
+        if(globalI>=arrayI.length)return true;
         if(arrayI[globalI]=='0'){
             while(j<arrayJ.length){
                 if(arrayJ[j]=='A'){
-                    postArray[j++]='x';
+                    postArray[j]='x';//++
                     if(!startFound){
                         startOfNewInterval=j;
                         startFound=true;
@@ -33,21 +55,25 @@ public class Main {
                         break;
                     }
                 }
+                j++;
             }
-            return canConvert(postArray,startOfNewInterval,j) ;
+            if(globalI==arrayI.length-1 && j==arrayJ.length-1)return true;
+            return canConvert(postArray,startOfNewInterval,j-1);
         }else{
-            int wasA=0;
-            int wasB=0;
             char lastChar=' ';
             while(j<arrayJ.length){
                 if(j<=lastXPositionOfReceiveArray+1){
-
+                    lastChar=arrayJ[j];
                 }else{
-
+                    if(arrayJ[j]!=lastChar){
+                        break;
+                    }
                 }
+                postArray[j]='x';
+                j++;
             }
-
+            if(globalI==arrayI.length-1 && j==arrayJ.length-1)return true;
+            return canConvert(postArray,firstXPositionOfReceiveArray+1,j-1);
         }
-        return false;
     }
 }
