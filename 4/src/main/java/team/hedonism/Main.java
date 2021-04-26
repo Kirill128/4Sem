8f4c523a6c01cca1a00ba622d9f0c7020e5437ba
Kirill128 <56486184+Kirill128@users.noreply.github.com>
2021-04-26 20:35:50 +0300

    Delete 4 directory

--- 4/src/main/java/team/hedonism/Main.java
+++
@@ -1,8 +0,0 @@
-package team.hedonism;
-
-public class Main {
-    public static boolean DEBUG = true;
-    public static int PHILOSOPHERS_COUNT = 6;
-    public static int MAX_WAIT_MS = 2000;
-    public static int DINNER_DURATION_IN_MS = 5000;
-}
